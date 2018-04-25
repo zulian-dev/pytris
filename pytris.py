606531c57a9d0c3dad5c854bd2341d46ceaf4e4e
zulianlf <lezk@ymail.com>
2018-04-25 13:43:26 -0300

    S

--- pytris.py
+++ pytris.py
@@ -4,6 +4,7 @@
 from pprint import pprint 
 import time
 import os
+import sys
 
 from random import randint
 
@@ -109,20 +110,67 @@ class pytrix:
         
         self.px = 0
         self.py = 0
+        self.pt = ''
         
         self.bg = {}
         self.pc = {}
         
         self.P = 0
         
-        self.wall   = bcolors.Purple +  u'█'*2 + bcolors.ENDC
-        self.brick  = bcolors.Green  +  u'█'*2 + bcolors.ENDC
+        self.brick  = u'█'*2 
+        self.wall   = bcolors.Cyan +  u'█'*2 + bcolors.ENDC
         self.blank  = bcolors.Black +  u'█'*2 + bcolors.ENDC
         
-        pices = {
-            'squere' : '' ,
-            'long'   : '' ,
-            'Z'      : '' ,
+        self.pices = {
+            #'squere' : [[0,1], [0,1]] 
+            'O' : {
+                'up':{
+                    0:[0,1],
+                    1:[0,1]
+                },
+                'color':bcolors.Red
+            }, 
+            'I' : {
+                'up':{
+                    0:[0],
+                    1:[0],
+                    2:[0],
+                    3:[0]
+                },
+                'color':bcolors.Green
+            },
+            'L' : {
+                'up':{
+                    0:[0],
+                    1:[0],
+                    2:[0,1],
+                },
+                'color':bcolors.Yellow
+            },
+            'Z' : {
+                'up':{
+                    0:[0,1],
+                    1:[1,2]
+                },
+                'color':bcolors.Blue
+            },
+            'S' : {
+                'up':{
+                    0:[2,3],
+                    1:[1,2]
+                },
+                'color':bcolors.Purple
+            },
+            'T' : {
+                'up':{
+                    0:[1,2,3],
+                    1:[2]
+                },
+                'color':bcolors.White
+            }
+            
+            #'long'   :  ,
+            #'Z'      : '' ,
         }
         
         
@@ -139,10 +187,13 @@ class pytrix:
     def startCene(self):
         for y in range(0,self.Y):
             self.bg[y] = {} 
+            self.pc[y] = {} 
             for x in range(0, self.X):
                 if x == 0 or x == self.X-1 or y == self.Y-1:
                     self.bg[y][x] = self.wall
+                    self.pc[y][x] = self.blank
                 else:
+                    self.pc[y][x] = self.blank
                     self.bg[y][x] = self.blank
                 
             
@@ -151,13 +202,20 @@ class pytrix:
     def render(self):
         cenario=''
         cenario+=("\n")
+        
         for y in range(0, self.Y):
             for x in range(0, self.X):
-                cenario+=(self.bg[y][x])
+                
+                if self.pc[y][x] != self.blank:
+                    cenario+=self.pc[y][x]
+                    self.pc[y][x] = self.blank
+                else:
+                    cenario+=self.bg[y][x]
+                
             cenario+=("\n")
-            
-        cenario+=("Pontos: " + str(self.P))
         
+        cenario+=("Pontos: " + str(self.P))
+        self.clear()
         print(cenario)
         
         
@@ -169,18 +227,34 @@ class pytrix:
         
         self.py+=1
         
+        pice = self.pices[self.pt]
+        
+        for linha in pice['up']:
+            for pedaco in pice['up'][linha]:
+                if self.bg[self.py+linha][self.px+pedaco] == self.blank:
+                    self.pc[self.py+linha][self.px+pedaco] = pice['color'] + self.brick + bcolors.ENDC
+                else:
+                    self.verifyPontos()
+                    self.novaPeca()
+        
+        '''
         if self.bg[self.py][self.px] == self.blank:
             self.bg[self.py-1][self.px] = self.blank
-            #self.bg[self.py][self.px] = '@@'
             self.bg[self.py][self.px] = self.brick
         else:
             self.verifyPontos()
             self.novaPeca()
-        
+        '''
         
         
     def novaPeca(self):
-        self.px = randint(1, self.X-1)
+        pices = ['O','I','L','Z','S','T']
+        
+        self.pt =pices[ randint(0, len(pices)-1) ]
+        
+        
+        self.px = int(self.X / 2)
+        
         self.py = 0
         
     
@@ -211,18 +285,26 @@ class pytrix:
                     yy-=1
                     
                 
-            
+        
+    
+    def getKey(self):
+        # key = sys.stdin.readline()
+        # print(key)
+        pass
+        
         
     
     def clock(self):
         #clock = 0
         while True:
-            self.clear()
-            self.render()
+            #self.clear()
+            self.getKey()
+            # self.render()
             #self.verifyPontos()
             self.calcNext()
             #input('?')
             time.sleep(.6)
+            
 
 
 
