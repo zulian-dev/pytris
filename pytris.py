16992ed1619040cb73a1ff868a6b03e3f896cabf
zulianlf <lezk@ymail.com>
2018-04-25 18:44:22 -0300

    SS

--- pytris.py
+++ pytris.py
@@ -7,85 +7,108 @@ import os
 import sys
 
 from random import randint
+import msvcrt
+import _thread 
+
+import multiprocessing 
+import time
+
+import terminal
+
+
+
+
+os.system('cls')
+
+
+
+terminal.move(0, 0)
+
+print (terminal.RED)
+print (u'this is red')
+print (terminal.RESET)
+
+terminal.move(0, 0)
+
+print (terminal.GREEN)
+print (u'this is red')
+print (terminal.RESET)
+
+
+exit()
 
 
 class bcolors:
-    Black="\033[0;30m"        # Black
-    Red="\033[0;31m"          # Red
-    Green="\033[0;32m"        # Green
-    Yellow="\033[0;33m"       # Yellow
-    Blue="\033[0;34m"         # Blue
-    Purple="\033[0;35m"       # Purple
-    Cyan="\033[0;36m"         # Cyan
-    White="\033[0;37m"        # White
-
+    Black      ="\033[0;30m"    # Black
+    Red        ="\033[0;31m"    # Red
+    Green      ="\033[0;32m"    # Green
+    Yellow     ="\033[0;33m"    # Yellow
+    Blue       ="\033[0;34m"    # Blue
+    Purple     ="\033[0;35m"    # Purple
+    Cyan       ="\033[0;36m"    # Cyan
+    White      ="\033[0;37m"    # White
+    
     # Bold
-    BBlack="\033[1;30m"       # Black
-    BRed="\033[1;31m"         # Red
-    BGreen="\033[1;32m"       # Green
-    BYellow="\033[1;33m"      # Yellow
-    BBlue="\033[1;34m"        # Blue
-    BPurple="\033[1;35m"      # Purple
-    BCyan="\033[1;36m"        # Cyan
-    BWhite="\033[1;37m"       # White
-
+    BBlack     ="\033[1;30m"    # Black
+    BRed       ="\033[1;31m"    # Red
+    BGreen     ="\033[1;32m"    # Green
+    BYellow    ="\033[1;33m"    # Yellow
+    BBlue      ="\033[1;34m"    # Blue
+    BPurple    ="\033[1;35m"    # Purple
+    BCyan      ="\033[1;36m"    # Cyan
+    BWhite     ="\033[1;37m"    # White
+    
     # Underline
-    UBlack="\033[4;30m"       # Black
-    URed="\033[4;31m"         # Red
-    UGreen="\033[4;32m"       # Green
-    UYellow="\033[4;33m"      # Yellow
-    UBlue="\033[4;34m"        # Blue
-    UPurple="\033[4;35m"      # Purple
-    UCyan="\033[4;36m"        # Cyan
-    UWhite="\033[4;37m"       # White
-
+    UBlack     ="\033[4;30m"    # Black
+    URed       ="\033[4;31m"    # Red
+    UGreen     ="\033[4;32m"    # Green
+    UYellow    ="\033[4;33m"    # Yellow
+    UBlue      ="\033[4;34m"    # Blue
+    UPurple    ="\033[4;35m"    # Purple
+    UCyan      ="\033[4;36m"    # Cyan
+    UWhite     ="\033[4;37m"    # White
+    
     # Background
-    On_Black="\033[40m"       # Black
-    On_Red="\033[41m"         # Red
-    On_Green="\033[42m"       # Green
-    On_Yellow="\033[43m"      # Yellow
-    On_Blue="\033[44m"        # Blue
-    On_Purple="\033[45m"      # Purple
-    On_Cyan="\033[46m"        # Cyan
-    On_White="\033[47m"       # White
-
+    On_Black   ="\033[40m"      # Black
+    On_Red     ="\033[41m"      # Red
+    On_Green   ="\033[42m"      # Green
+    On_Yellow  ="\033[43m"      # Yellow
+    On_Blue    ="\033[44m"      # Blue
+    On_Purple  ="\033[45m"      # Purple
+    On_Cyan    ="\033[46m"      # Cyan
+    On_White   ="\033[47m"      # White
+    
     # High Intensty
-    IBlack="\033[0;90m"       # Black
-    IRed="\033[0;91m"         # Red
-    IGreen="\033[0;92m"       # Green
-    IYellow="\033[0;93m"      # Yellow
-    IBlue="\033[0;94m"        # Blue
-    IPurple="\033[0;95m"      # Purple
-    ICyan="\033[0;96m"        # Cyan
-    IWhite="\033[0;97m"       # White
-
+    IBlack     ="\033[0;90m"    # Black
+    IRed       ="\033[0;91m"    # Red
+    IGreen     ="\033[0;92m"    # Green
+    IYellow    ="\033[0;93m"    # Yellow
+    IBlue      ="\033[0;94m"    # Blue
+    IPurple    ="\033[0;95m"    # Purple
+    ICyan      ="\033[0;96m"    # Cyan
+    IWhite     ="\033[0;97m"    # White
+    
     # Bold High Intensty
-    BIBlack="\033[1;90m"      # Black
-    BIRed="\033[1;91m"        # Red
-    BIGreen="\033[1;92m"      # Green
-    BIYellow="\033[1;93m"     # Yellow
-    BIBlue="\033[1;94m"       # Blue
-    BIPurple="\033[1;95m"     # Purple
-    BICyan="\033[1;96m"       # Cyan
-    BIWhite="\033[1;97m"      # White
-
+    BIBlack    ="\033[1;90m"    # Black
+    BIRed      ="\033[1;91m"    # Red
+    BIGreen    ="\033[1;92m"    # Green
+    BIYellow   ="\033[1;93m"    # Yellow
+    BIBlue     ="\033[1;94m"    # Blue
+    BIPurple   ="\033[1;95m"    # Purple
+    BICyan     ="\033[1;96m"    # Cyan
+    BIWhite    ="\033[1;97m"    # White
+    
     # High Intensty backgrounds
-    On_IBlack="\033[0;100m"   # Black
-    On_IRed="\033[0;101m"     # Red
-    On_IGreen="\033[0;102m"   # Green
-    On_IYellow="\033[0;103m"  # Yellow
-    On_IBlue="\033[0;104m"    # Blue
-    On_IPurple="\033[10;95m"  # Purple
-    On_ICyan="\033[0;106m"    # Cyan
-    On_IWhite="\033[0;107m"   # White
+    On_IBlack  ="\033[0;100m"   # Black
+    On_IRed    ="\033[0;101m"   # Red
+    On_IGreen  ="\033[0;102m"   # Green
+    On_IYellow ="\033[0;103m"   # Yellow
+    On_IBlue   ="\033[0;104m"   # Blue
+    On_IPurple ="\033[10;95m"   # Purple
+    On_ICyan   ="\033[0;106m"   # Cyan
+    On_IWhite  ="\033[0;107m"   # White
     
     ENDC = '\033[0m'
-
-
-
-
-
-
 
 
 
@@ -179,9 +202,34 @@ class pytrix:
         self.startCene()
         self.novaPeca()
         
+        #teclado = myThread(1, "Thread1", self)
+        #teclado.start()
+        _thread.start_new_thread( self.teclado, () )
         
     def clear(self):
+        # print ('\033[' + str(0) + ';' + str(0) + 'H')
         os.system('cls')
+    
+    
+    def teclado(self):
+        while True:
+            
+            key = msvcrt.getch()
+            
+            if key == b'K':
+                self.px-=1
+                self.renderPice()
+                self.render()
+            
+            elif key == b'M':
+                self.px+=1
+                self.renderPice()
+                self.render()
+            
+            elif key == b'\x1b':
+                quit()
+
+
     
     
     def startCene(self):
@@ -215,17 +263,25 @@ class pytrix:
             cenario+=("\n")
         
         cenario+=("Pontos: " + str(self.P))
+        
         self.clear()
         print(cenario)
+        # print ("\x1b[31;4mHello\x1b[0m")
+        
+        # print ('A')
+        
         
         
         
     def calcNext(self):
+        self.py+=1
+        self.renderPice()
+    
+    def renderPice(self):
         
         if self.px == 0 and self.py == 0:
             return
         
-        self.py+=1
         
         pice = self.pices[self.pt]
         
@@ -234,19 +290,16 @@ class pytrix:
                 if self.bg[self.py+linha][self.px+pedaco] == self.blank:
                     self.pc[self.py+linha][self.px+pedaco] = pice['color'] + self.brick + bcolors.ENDC
                 else:
+                    for linha in pice['up']:
+                        for pedaco in pice['up'][linha]:
+                            self.bg[self.py+linha-1][self.px+pedaco] = pice['color'] + self.brick + bcolors.ENDC
+                    
                     self.verifyPontos()
                     self.novaPeca()
+                
+            
         
-        '''
-        if self.bg[self.py][self.px] == self.blank:
-            self.bg[self.py-1][self.px] = self.blank
-            self.bg[self.py][self.px] = self.brick
-        else:
-            self.verifyPontos()
-            self.novaPeca()
-        '''
-        
-        
+    
     def novaPeca(self):
         pices = ['O','I','L','Z','S','T']
         
@@ -288,18 +341,25 @@ class pytrix:
         
     
     def getKey(self):
-        # key = sys.stdin.readline()
-        # print(key)
-        pass
+        import msvcrt
+        key = msvcrt.getch()
         
+        if key == b'K':
+            self.px-=1
+        
+        elif key == b'M':
+            self.px+=1
+        
+        
+        pass
         
     
     def clock(self):
         #clock = 0
         while True:
             #self.clear()
-            self.getKey()
-            # self.render()
+            # self.getKey()
+            self.render()
             #self.verifyPontos()
             self.calcNext()
             #input('?')
